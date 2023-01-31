b14068af50c6e491edfbc9f5a8695ef2b958777e
Diana <aleksodiana@gmail.com>
2023-01-31 09:57:27 -0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ function App() {
   return (
 
        <Routes>
-        <Route exact  path="*" element={<Layout name={'Spectacles Women'} />}>
+        <Route exact  path="/" element={<Layout name={'Spectacles Women'} />}>
           <Route index path="/spectacles-women" element={<Layout/>} />
           <Route path="/spectacles-men" element={<Layout name={'Spectacles Men'} />} />
           <Route path="/sunglasses-women" element={<Layout name={'Sunglasses Women'} />} />
           <Route path="/sunglasses-men" element={<Layout name={'Sunglasses Men'} />} />
-          <Route path="*" element={<p>Not found!</p>} />
+          {/* <Route path="*" element={<p>Not found!</p>} /> */}
         </Route>
       </Routes>
 
